a3529757d2138deb418acbcfc47c2d6820e3cf14
nova-sangeeth <novasangeeth@outlook.com>
2020-06-04 18:32:22 +0530

    added new templates and views.

--- app_main/urls.py
+++ app_main/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, listing, home_feed, test_view
 
 
-urlpatterns=[
+urlpatterns = [
     url(r'^$', index, name='index'),
+    # url(r'^$', , name='')
+    url(r'^test$', test_view, name='test_view'),
+    url(r'^home$', home_feed, name='home_feed')
 ]
--- app_main/views.py
+++ app_main/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from .models import Question, Answer
+# from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def home_feed(request):
+    current_user = request.user
+    questions = Question.objects.filter(points_gt=-2).order_by('-created')
+    paginator = Paginator(questions, 15)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'home.html')
+
+
+def listing(request):
+    return render(request, 'list.html', {'page_obj': page_obj})
+
+
+def test_view(request):
+    current_user = request.user
+
+    return render(request, 'test.html', {'username': current_user.username})
