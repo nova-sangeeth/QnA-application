f853f5e178ca9ce9b1c0dbac59e2c6f3390eeef2
nova-sangeeth <novasangeeth@outlook.com>
2020-07-11 22:33:08 +0530

    added congfig for postgres in the settings

--- QnA_app/settings.py
+++ QnA_app/settings.py
@@ -93,6 +93,16 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+# CONFIG FOR POSTGRESQL.
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         'USER':
+#         'PASSWORD':
+
+#     }
+# }
 
 
 # Password validation
