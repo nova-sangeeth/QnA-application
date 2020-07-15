6e13beb5fa9cbf7434eaa744e881b8af85f70a75
nova-sangeeth <novasangeeth@outlook.com>
2020-07-16 00:47:46 +0530

    added new model user_profile for the application.

--- users/models.py
+++ users/models.py
@@ -2,11 +2,28 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from main.models import Question
 
+from django.contrib.auth.models import User
+
+# from django.contrib.auth import get_user_model
+
+# User = get_user_model()
+
 
 class User(AbstractUser):
     class Meta:
-        db_table = 'auth_user'
-    upvoted_questions = models.ManyToManyField(
-        Question, related_name="upvoted_users")
+        db_table = "auth_user"
+
+    upvoted_questions = models.ManyToManyField(Question, related_name="upvoted_users")
     downvoted_questions = models.ManyToManyField(
-        Question, related_name="downvoted_users")
+        Question, related_name="downvoted_users"
+    )
+
+
+class user_profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    Country = models.CharField(max_length=64, null=True)
+    Phone = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.user
+
