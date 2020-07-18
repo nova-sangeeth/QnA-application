a2ee0a7ed3b4e5dcbadf91dc52464e1a9d8d21fc
nova-sangeeth <novasangeeth@outlook.com>
2020-07-19 00:30:33 +0530

    added new serializers for the application main .

--- main/serializers.py
+++ main/serializers.py
@@ -1,15 +1,36 @@
 from rest_framework import serializers
 from .models import Answer, Question
+from django.utils.html import escape
 
 
-class question_serializer(serializers.ModelSerializer):
+class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ("user", "title", "body", "created", "answers_count", "points")
 
 
-class answer_serializer(serializers.ModelSerializer):
+class CreatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        diff = timezone.now() - value
+        return x_ago_helper(diff)
+
+
+class UserField(serializers.Field):
+    def to_representation(self, value):
+        return value.username
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    user = UserField()
+    x_ago = serializers.SerializerMethodField()
+    text_html = serializers.SerializerMethodField()
+
     class Meta:
         model = Answer
-        fields = ("text", "created", "modified", "points")
+        fields = ("text_html", "x_ago", "user", "id", "points", "hidden")
 
+    def get_text_html(self, obj):
+        return urlize(escape(obj.text))
+
+    def get_x_ago(self, obj):
+        return x_ago_helper(timezone.now() - obj.created)
