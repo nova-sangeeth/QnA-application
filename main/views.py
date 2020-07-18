85682d80d846cabdfa9fe1c4f0db7f74b839a1d5
nova-sangeeth <novasangeeth@outlook.com>
2020-07-19 00:31:57 +0530

    renamed the views for better readablity and similarity to the original source.

--- main/views.py
+++ main/views.py
@@ -17,29 +17,27 @@ def profile(request):
     return render(request, "profile.html", {"data": user})
 
 
-def home_feed(request):
+def homeFeedView(request):
     current_user = request.user
-    questions = Question.objects.filter(points__gt=-2).order_by("-created")
-    paginator = Paginator(questions, 15)
+
+    questions = Question.objects.filter(points__gt=-2, hidden=False).order_by(
+        "-created"
+    )
+    paginator = Paginator(questions, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+
     questions_exist = len(questions) > 0
-    return render(
-        request,
-        "home.html",
-        {
-            "current_user": current_user,
-            "page_obj": page_obj,
-            "questions_exist": questions_exist,
-        },
-    )
+    context = {
+        "current_user": current_user,
+        "page_obj": page_obj,
+        "questions_exist": questions_exist,
+    }
+    return render(request, "home.html", context)
 
 
-def listing(request):
-    return render(request, "list.html", {"page_obj": page_obj})
-
-
-def test_view(request):
+def testView(request):
     current_user = request.user
+    context = {"username": current_user.username, "current_user": current_user}
+    return render(request, "test.html", context)
 
-    return render(request, "test.html", {"username": current_user.username})
