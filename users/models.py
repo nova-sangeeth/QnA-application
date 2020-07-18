9c8395e459414c2906b8ed57a90a567b9b7f3461
nova-sangeeth <novasangeeth@outlook.com>
2020-07-19 00:37:21 +0530

    added new fields for points, adjustment_points and is shadow baned.

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from main.models import Question, Answer
-
+from django.db.models import Q
 from django.contrib.auth.models import User
+from functools import reduce
+
 
 # from django.contrib.auth import get_user_model
 
@@ -19,6 +21,16 @@ class User(AbstractUser):
     )
     upvoted_answers = models.ManyToManyField(Answer, related_name="upvoted_users")
     downvoted_answers = models.ManyToManyField(Answer, related_name="downvoted_users")
+    points = models.IntegerField(default=0)
+    adjustment_points = models.IntegerField(default=0)
+    is_shadow_banned = models.BooleanField(default=False)
+
+    def update_points(self):
+        answers = self.answer_set.filter(~Q(points=0))
+        points = map(lambda a: a.points, answers)
+        user_points = reduce(lambda x, y: x + y, points, 0)
+        self.points = user_points + self.adjustment_points
+        self.save()
 
 
 class user_profile(models.Model):
