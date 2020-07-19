c12d0f3b157342925790488f7d541b3cef50b3cc
nova-sangeeth <novasangeeth@outlook.com>
2020-07-19 21:31:29 +0530

    added a alternative route for the templates.

--- QnA_app/settings.py
+++ QnA_app/settings.py
@@ -69,6 +69,9 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            # os.path.join(os.path.dirname(__file__), "templates").replace("\\", "/")
+        ],
         "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
